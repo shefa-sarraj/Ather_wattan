76d24bf4574f85a8f29139109e786a1c7e68dc6f
shefa <shefaalsaraj2020@gmail.com>
2026-07-19 00:58:51 +0300

    Bypass DB connection crash for UI deployment

--- api/connection.php
+++ api/connection.php
@@ -4,15 +4,14 @@ $username = "root";
 $password = "";
 $dbname = "palestine_heritage";
 
-// إنشاء الاتصال
-$conn = new mysqli($servername, $username, $password, $dbname);
+// إنشاء الاتصال مع كتم الأخطاء مؤقتاً باستخدام علامة @
+$conn = @new mysqli($servername, $username, $password, $dbname);
 
-// التحقق من الاتصال
+// التحقق من الاتصال دون التسبب في انهيار السيرفر (Bypass die)
 if ($conn->connect_error) {
-    die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
+    // تركنا الكود فارغاً هنا مؤقتاً ليسمح للواجهة بالظهور ولا يوقف السيرفر
+} else {
+    // لدعم اللغة العربية في حال نجاح الاتصال مستقبلاً
+    $conn->set_charset("utf8");
 }
-
-//لدعم اللغه العربيه
-$conn->set_charset("utf8");
-
 ?>
