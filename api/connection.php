793d9c2c69ab165cef1d8ab8a416d7863009e08c
shefa <shefaalsaraj2020@gmail.com>
2026-07-19 23:25:00 +0300

    Deploy final database configuration to production

--- api/connection.php
+++ api/connection.php
@@ -1,17 +1,1 @@
-<?php
-$servername = "blaxdyjfxqxgu2ivmnip-mysql.services.clever-cloud.com";
-$username = "uset8nkvjyvcnqm2";
-$password = "NMQPAAlrgBNtQmMewFUN"; 
-$dbname = "blaxdyjfxqxgu2ivmnip";
-
-// إنشاء الاتصال السحابي الحقيقي
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// التحقق من الاتصال
-if ($conn->connect_error) {
-    die("فشل الاتصال بقاعدة البيانات السحابية: " . $conn->connect_error);
-}
-
-// لدعم اللغة العربية بشكل صحيح
-$conn->set_charset("utf8");
-?>
+NMQPAAlrgBNtQmMewFUN
